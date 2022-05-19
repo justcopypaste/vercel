1959a64bd6c205e14d92ac3beaad3eaf221e96c3
Joako Caballero <joako@Joakos-Mac-Pro.local>
2022-05-18 21:20:00 -0300

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./server');
 
 const host = "http://localhost";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3080;
 
 app.listen(port, () => {
     console.log(`Servidor iniciado en ${host}:${port}`);
